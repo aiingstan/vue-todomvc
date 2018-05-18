e2672dece0822c50be53983cea3666ee9e4f43c5
Yufeng Guo <guo_yufeng@outlook.com>
2018-05-18 23:33:12 +0800

    finish index.html

---
+++ README.md
@@ -0,0 +1,18 @@
+[TodoMVC](https://github.com/vuejs/vue/blob/dev/examples/todomvc/app.js)
+
+what is `data-framework` declaration
+
+`direct.js` is a front-end router library
+
+`[v-cloak]` is property selector like `input[type=email]`
+
+`autofocus` property
+
+bind event `keyup.enter`
+
+conditioning bind CSS class name
+
+way to use `localStorage`
+
+
+
---
+++ app.js
@@ -0,0 +1,51 @@
+var STORAGE_KEY = 'todos-vuejs-2.0';
+
+var todoStorage = {
+  fetch: function () {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+    todos.forEach(function (todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
+// visibility filters
+var filters = {
+  all: function (todos) {
+    return todos;
+  },
+  active: function (todos) {
+    return todos.filter(function (todo) {
+      return !todo.completed
+    })
+  },
+  completed: function (todos) {
+    return todos.filter(function (todo) {
+      return todo.completed
+    })
+  }
+}
+
+// app Vue instance
+var app = new Vue({
+  data: {
+    todos: todoStorage.fetch(),
+    newTodo: '',
+    editedTodo: null,
+    visibility: 'all'
+  },
+
+  watch: {
+    todos: {
+      handler: function (todos) {
+        todoStorage.save(todos)
+      },
+      deep: true
+    }
+  }
+})
