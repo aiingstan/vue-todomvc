e8d1cd1a8d48f721f90a2918bb9f34cde74ef53b
Yufeng Guo <guo_yufeng@outlook.com>
2018-05-19 15:06:20 +0800

    learned

--- README.md
+++ README.md
@@ -1,18 +1,31 @@
-[TodoMVC](https://github.com/vuejs/vue/blob/dev/examples/todomvc/app.js)
+Take-aways from [TodoMVC](https://github.com/vuejs/vue/blob/dev/examples/todomvc/app.js) example
 
-what is `data-framework` declaration
+what is `data-framework` data declaration and why
+[Question on SegmentFault.com](https://segmentfault.com/q/1010000014933169?_ea=3750845)
 
-`direct.js` is a front-end router library
+`director.js` is a router library
 
-`[v-cloak]` is property selector like `input[type=email]`
+`[v-cloak]` is property selector, not specifying any tag
 
-`autofocus` property
+`autofocus` property for `input` element
+only `HTML5`, and only one input element of the document can have this property
 
 bind event `keyup.enter`
+[系统修饰键](https://cn.vuejs.org/v2/guide/events.html#%E7%B3%BB%E7%BB%9F%E4%BF%AE%E9%A5%B0%E9%94%AE)
 
 conditioning bind CSS class name
+[动态绑定 Class 的对象语法](https://cn.vuejs.org/v2/guide/class-and-style.html#%E5%AF%B9%E8%B1%A1%E8%AF%AD%E6%B3%95)
 
 way to use `localStorage`
+[localStorage](https://developer.mozilla.org/en-US/docs/Web/API/Storage/LocalStorage)
+notice `private mode`
+
+
+use `directives`
+
+use `filters`
+
+`box-shadow`
 
 
 
--- app.js
+++ app.js
@@ -36,6 +36,7 @@ var app = new Vue({
   data: {
     todos: todoStorage.fetch(),
     newTodo: '',
+    beforeEditCache: '',
     editedTodo: null,
     visibility: 'all'
   },
@@ -47,5 +48,102 @@ var app = new Vue({
       },
       deep: true
     }
+  },
+
+  // computed properties
+  computed: {
+    filteredTodos: function () {
+      return filters[this.visibility](this.todos)
+    },
+    remaining: function () {
+      return filters.active(this.todos).length
+    },
+    allDone: {
+      get: function () {
+        return this.remaining === 0;
+      },
+      set: function (value) {
+        this.todos.forEach(function (todo) {
+          todo.completed = value;
+        })
+      }
+    }
+  },
+
+  filters: {
+    pluralize: function (n) {
+      return n === 1 ? 'item' : 'items'
+    }
+  },
+
+  methods: {
+    addTodo: function () {
+      var value = this.newTodo && this.newTodo.trim();
+      if (!value) {
+        return;
+      }
+      this.todos.push({
+        id: todoStorage.uid++,
+        title: value,
+        completed: false
+      });
+      this.newTodo = '';
+    },
+
+    removeTodo: function (todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1)
+    },
+
+    editTodo: function (todo) {
+      console.log('edit todo');
+      this.beforeEditCache = todo.title
+      this.editedTodo = todo
+    },
+
+    doneEdit: function (todo) {
+      console.log('done edit');
+      if (!this.editedTodo) {
+        return;
+      }
+      this.editedTodo = null;
+      todo.title = todo.title.trim();
+      if (!todo.title) {
+        this.removeTodo(todo);
+      }
+    },
+
+    cancelEdit: function (todo) {
+      this.editedTodo = null;
+      todo.title = this.beforeEditCache
+    },
+
+    removeCompleted: function () {
+      this.todos = filters.active(this.todos);
+    }
+  },
+
+  directives: {
+    'todo-focus': function (el, binding) {
+      console.log('todo focus', el, binding);
+      if (binding.value) {
+        el.focus();
+      }
+    }
   }
 })
+
+function onHashChange () {
+  var visibility = window.location.hash.replace(/#\/?/, '')
+  if (filters[visibility]) {
+    app.visibility = visibility;
+  }
+  else {
+    window.location.hash = ''
+    app.visibility = 'all'
+  }
+}
+
+window.addEventListener('hashchange', onHashChange)
+onHashChange()
+
+app.$mount('.todoapp')
